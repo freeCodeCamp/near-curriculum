7d0a3501dea68942a7cf7513df689f84b6768d86
Shaun Hamilton <shauhami020@gmail.com>
2022-09-07 13:52:56 +0100

    feat: add freecodecamp-os as package

---
+++ .freeCodeCamp/tooling/env.js
@@ -0,0 +1,99 @@
+import { readFile, writeFile } from 'fs/promises';
+import { join } from 'path';
+
+export const ROOT = process.env.INIT_CWD || process.cwd();
+
+export async function getConfig() {
+  console.log('ROOT: ', ROOT);
+  const config = await readFile(join(ROOT, 'freecodecamp.conf.json'), 'utf-8');
+  return JSON.parse(config);
+}
+
+export const freeCodeCampConfig = await getConfig();
+
+export async function getState() {
+  let defaultState = {
+    currentProject: null,
+    locale: 'english'
+  };
+  try {
+    const state = JSON.parse(
+      await readFile(
+        join(ROOT, freeCodeCampConfig.config['state.json']),
+        'utf-8'
+      )
+    );
+    return { ...defaultState, ...state };
+  } catch (err) {
+    console.error(err);
+  }
+  return defaultState;
+}
+
+export async function setState(obj) {
+  const state = await getState();
+  const updatedState = {
+    ...state,
+    ...obj
+  };
+
+  await writeFile(
+    join(ROOT, freeCodeCampConfig.config['state.json']),
+    JSON.stringify(updatedState, null, 2)
+  );
+}
+
+/**
+ * @param {string} project Project dashed name
+ */
+export async function getProjectConfig(project) {
+  const projects = JSON.parse(
+    await readFile(
+      join(ROOT, freeCodeCampConfig.config['projects.json']),
+      'utf-8'
+    )
+  );
+
+  const proj = projects.find(p => p.dashedName === project);
+
+  const defaultConfig = {
+    testPollingRate: 333,
+    currentLesson: 1,
+    runTestsOnWatch: false,
+    lastKnownLessonWithHash: 1,
+    seedEveryLesson: false,
+    useGitBuildOnProduction: false // TODO: Necessary?
+  };
+  if (!proj) {
+    return defaultConfig;
+  }
+  return { ...defaultConfig, ...proj };
+}
+
+/**
+ *
+ * @param {string} project Project dashed name
+ * @param {object} config Config properties to set
+ */
+export async function setProjectConfig(project, config = {}) {
+  const projects = JSON.parse(
+    await readFile(
+      join(ROOT, freeCodeCampConfig.config['projects.json']),
+      'utf-8'
+    )
+  );
+
+  const updatedProject = {
+    ...projects.find(p => p.dashedName === project),
+    ...config
+  };
+
+  const updatedProjects = projects.map(p =>
+    p.dashedName === project ? updatedProject : p
+  );
+
+  await writeFile(
+    join(ROOT, freeCodeCampConfig.config['projects.json']),
+    JSON.stringify(updatedProjects, null, 2)
+  );
+}
---
+++ .freeCodeCamp/tooling/hot-reload.js
@@ -0,0 +1,57 @@
+// This file handles the watching of the /curriculum folder for changes
+// and executing the command to run the tests for the next (current) lesson
+import { getState, getProjectConfig, ROOT } from './env.js';
+import runLesson from './lesson.js';
+import runTests from './test.js';
+import { watch } from 'chokidar';
+import { join } from 'path';
+const { currentProject } = await getState();
+const { testPollingRate, runTestsOnWatch } = await getProjectConfig(
+  currentProject
+);
+
+function hotReload(ws) {
+  console.log(`Watching for file changes on ${ROOT}`);
+  let isWait = false;
+  let testsRunning = false;
+  let isClearConsole = false;
+
+  const pathsToIgnore = [
+    '.logs/.temp.log',
+    'config/',
+    '/node_modules/',
+    '.git',
+    '/target/',
+    '/test-ledger/'
+  ];
+
+  watch(ROOT, {
+    ignoreInitial: true
+    // `ignored` appears to do nothing. Have tried multiple permutations
+    // ignored: pathsToIgnore.join('|') //p => pathsToIgnore.includes(p)
+  }).on('all', async (event, name) => {
+    if (name && !pathsToIgnore.find(p => name.includes(p))) {
+      if (isWait) return;
+      isWait = setTimeout(() => {
+        isWait = false;
+      }, testPollingRate);
+
+      const { currentProject } = await getState();
+      if (!currentProject) {
+        return;
+      }
+      if (isClearConsole) {
+        console.clear();
+      }
+      await runLesson(ws, currentProject);
+      if (runTestsOnWatch && !testsRunning) {
+        console.log(`Watcher: ${event} - ${name}`);
+        testsRunning = true;
+        await runTests(ws, currentProject);
+        testsRunning = false;
+      }
+    }
+  });
+}
+
+export default hotReload;
