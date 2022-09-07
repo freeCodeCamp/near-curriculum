f737c911128932741f446df23e8da976b239665b
Shaun Hamilton <shauhami020@gmail.com>
2022-09-07 12:37:27 +0000

    feat: freecodecamp-os package

--- .freeCodeCamp/client/components/ruler.tsx
+++
@@ -1,9 +0,0 @@
-const rulerStyle = {
-  width: '80%',
-  height: '1px',
-  backgroundColor: '#ccc',
-  margin: '0 auto'
-};
-export const Ruler = () => {
-  return <div style={rulerStyle}></div>;
-};
